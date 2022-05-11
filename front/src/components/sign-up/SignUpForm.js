448b32f3ee40b5214f29b29b0e2b1e896a9bc5d3
C <earthhominid@gmail.com>
2022-05-11 17:45:37 -0300

    invalid styling added to email input

--- front/src/components/sign-up/SignUpForm.js
+++ front/src/components/sign-up/SignUpForm.js
@@ -2,21 +2,176 @@ import React from 'react';
 
 const SignUpForm = () => {
   return (
-    <form>
-      <label class="block">
-        <span class="block text-sm font-medium text-slate-700">Username</span>
-        <input
-          type="text"
-          value="tbone"
-          disabled
-          class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-        />
-      </label>
-    </form>
+    <>
+      <h1 className="px-2 mt-2 text-xl font-bold text-indigo-600 text-center">
+        Fill out the form to become a member!
+      </h1>
+      <div className="flex w-full justify-center ">
+        <div
+          className="
+        w-full 
+        max-w-md 
+        m-4
+        flex 
+        justify-center
+        bg-white 
+        border 
+        border-slate-300 
+        shadow-lg
+        rounded-md"
+        >
+          <form action="" method="POST" className="mt-6 text-sm ">
+            <div className="relative">
+              <input
+                id="first_name"
+                type="text"
+                name="first_name"
+                required=""
+                placeholder="Firstname"
+                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent
+                invalid:border-pink-500 
+                invalid:text-pink-600
+                focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
+              <label
+                for="first_name"
+                className="
+                text-stone-800 
+                absolute 
+                left-0  
+                -top-3.5 
+                text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-stone-600 peer-placeholder-shown:top-2 transition-all 
+                peer-focus:-top-3.5
+                peer-foucs:text-stone-600
+                peer-focus:text-sm"
+              >
+                Firstname
+              </label>
+            </div>
+            <div className="mt-6 relative">
+              <input
+                id="last_name"
+                name="last_name"
+                required=""
+                type="text"
+                placeholder="Lastname"
+                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent"
+              />
+              <label
+                for="last_name"
+                className="
+              absolute
+              left-0
+              -top-3.5
+              text-sm 
+              text-stone-800
+              transition-all
+              peer-placeholder-shown:text-base
+              peer-placeholder-shown:text-stone-600
+              peer-placeholder-shown:top-2
+              peer-focus:-top-3.5
+              peer-foucs:text-stone-600
+              peer-focus:text-sm"
+              >
+                Lastname
+              </label>
+            </div>
+            <div className="mt-6 relative">
+              <input
+                id="email"
+                name="email"
+                required=""
+                type="email"
+                placeholder="Username"
+                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent
+                invalid:border-pink-500 
+                invalid:text-pink-600
+                focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
+              <label
+                for="email"
+                className="
+              absolute
+              left-0
+              -top-3.5
+              text-sm 
+              text-stone-800
+              transition-all
+              peer-placeholder-shown:text-base
+              peer-placeholder-shown:text-stone-600
+              peer-placeholder-shown:top-2
+              peer-focus:-top-3.5
+              peer-foucs:text-stone-600
+              peer-focus:text-sm"
+              >
+                Email
+              </label>
+            </div>
+            <div className="mt-6 relative">
+              <input
+                id="username"
+                name="username"
+                required=""
+                type="text"
+                placeholder="Username"
+                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent"
+              />
+              <label
+                for="username"
+                className="
+              absolute
+              left-0
+              -top-3.5
+              text-sm 
+              text-stone-800
+              transition-all
+              peer-placeholder-shown:text-base
+              peer-placeholder-shown:text-stone-600
+              peer-placeholder-shown:top-2
+              peer-focus:-top-3.5
+              peer-foucs:text-stone-600
+              peer-focus:text-sm"
+              >
+                Username
+              </label>
+            </div>
+            <div className="mt-6 relative">
+              <input
+                id="password"
+                name="password"
+                required=""
+                type="text"
+                placeholder="Password"
+                className="peer h-10 w-full border-b-2 border-slate-400 focus:outline-none focus:border-indigo-600 placeholder-transparent"
+              />
+              <label
+                for="password"
+                className="
+              absolute
+              left-0
+              -top-3.5
+              text-sm 
+              text-stone-800
+              transition-all
+              peer-placeholder-shown:text-base
+              peer-placeholder-shown:text-stone-600
+              peer-placeholder-shown:top-2
+              peer-focus:-top-3.5
+              peer-foucs:text-stone-600
+              peer-focus:text-sm"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-10 mb-12">
+              <button className="bg-indigo-600 p-2 rounded-md text-white w-full text-base hover:opacity-90">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
