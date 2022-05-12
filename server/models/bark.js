ffc95623783292ecfc316f3766413120c88af924
C <earthhominid@gmail.com>
2022-05-12 17:14:26 -0300

    bark model added

---
+++ server/models/bark.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BarkSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Needs a title'],
+      minLength: 2,
+      maxLength: 25,
+    },
+    description: {
+      type: String,
+      required: [true, 'Needs a description'],
+      minLength: 2,
+      maxLength: 100,
+    },
+    howl: {
+      type: Schema.Types.ObjectId,
+      ref: 'Howl',
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+BarkSchema.virtual('url').get(function () {
+  return 'chatroom/bark' + this._id;
+});
+
+model.exports = mongoose.model('Bark', BarkSchema);
