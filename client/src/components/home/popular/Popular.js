62bc932a7c766d1c032ac9f4b038ea581a4a4167
C <earthhominid@gmail.com>
2022-05-12 11:06:17 -0300

    updated styling

--- client/src/components/home/popular/Popular.js
+++ client/src/components/home/popular/Popular.js
@@ -43,9 +43,11 @@ const Popular = () => {
                 <strong>b/barker</strong> User Name Posted May 8
               </h2>
               <h1 className="pt-1 pl-2 text-lg">This is the heading.</h1>
-              <h2 className="pt-1 pl-2 pb-2">This is the content.</h2>
+              <h2 className="pt-1 pl-2 pb-2">
+                This is the content. This is more content.
+              </h2>
               <div className="flex justify-end order-last flex-row w-full">
-                <img src={Talk} alt="paw" className="pb-1 w-6" />
+                <img src={Talk} alt="paw" className="pb-1 w-6 mr-1" />
               </div>
             </div>
           </div>
