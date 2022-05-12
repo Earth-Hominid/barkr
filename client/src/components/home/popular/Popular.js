313682ac9c0387b79520ec836f39a7b2e623daa2
C <earthhominid@gmail.com>
2022-05-12 02:04:53 -0300

    homepage design, tailwindcss added

--- client/src/components/home/popular/Popular.js
+++ client/src/components/home/popular/Popular.js
@@ -6,31 +6,44 @@ const Popular = () => {
     <>
       <div className="pt-4 mx-6 sm:mx-2">
         <div className="text-xs mb-1">Popular Howls</div>
-        <div
-          id="container"
-          className="border-gray-100
+        <div className="sm:flex w-full mt-1 sm:justify-center">
+          <div
+            id="container"
+            className="
+          border-solid
+          border-stone-300
+          border-x
+          border-y
           bg-white
           w-full
           flex
           flex-row
-          sm:max-w-md
+          sm:max-w-lg
           shadow-md
-          rounded-sm"
-        >
-          <div
-            className="w-8 h-full bg-blue-200
+          rounded-sm
+          cursor-pointer
+          hover:border-stone-900
+          hover:border-dotted
+          hover:border-2"
+          >
+            <div
+              className="w-8 h-100% bg-blue-100
             flex
             flex-col
             justify-center
             items-center"
-          >
-            <img className="w-5 mt-1 mb-1" src={Paw} alt="paw icon" />
-          </div>
+            >
+              <img className="w-5 mt-1 mb-1" src={Paw} alt="paw icon" />
+              <img className="w-5 mt-1 mb-1" src={Paw} alt="paw icon" />
+            </div>
 
-          <div>
-            <h2 className="ml-1 text-[5%]">
-              <strong>b/barker</strong> User Name Posted May 8
-            </h2>
+            <div>
+              <h2 className="ml-1 mt-1 text-[5%]">
+                <strong>b/barker</strong> User Name Posted May 8
+              </h2>
+              <h1 className="pt-1 pl-2 text-lg">This is the heading.</h1>
+              <h2 className="pt-1 pl-2 pb-2">This is the content.</h2>
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/home/trending/Trending.js
+++ client/src/components/home/trending/Trending.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import dogRetriever from '../../../assets/images/articles/ret.jpeg';
+import dogDrink from '../../../assets/images/articles/drink.png';
+import dogRun from '../../../assets/images/articles/dogRun.jpeg';
+import Paw from '../../../assets/icons/paw.svg';
+import Flower from '../../../assets/icons/flower.svg';
+import Drop from '../../../assets/icons/drop.svg';
 
 const Trending = () => {
   return (
@@ -7,25 +12,98 @@ const Trending = () => {
         <div className="text-xs mb-1">Trending Today</div>
 
         <div className="flex flex-wrap sm:flex-nowrap align-items justify-center">
-          <div className="bg-stone-700 max-w-xs p-3 rounded-md mx-1 my-1">
-            <h2 className="text-sm text-white">World News</h2>
-            <h3 className="text-xs text-white">
-              Rank 2022 Top breed is still Retrievers (Labrador)
-            </h3>
+          <div
+            className="
+          relative 
+          bg-stone-700 
+          shadow-md 
+          sm:max-w-[200px] 
+          max-w-[250px] 
+          p-3 
+          rounded-md 
+          mx-1 
+          my-1"
+          >
+            <img
+              src={dogRetriever}
+              alt="labrador"
+              className="
+              absolute 
+              inset-0 
+              h-full 
+              w-full 
+              object-cover
+              overflow-hidden
+              rounded-md
+              opacity-40
+              min-h-[80px]"
+            />
+            <div className="relative">
+              <h2 className="text-sm text-white font-bold mt-1">Rank 2022</h2>
+              <h3 className="text-[12px] text-white font-semi-bold">
+                Top breed is still Retrievers (Labrador)
+              </h3>
+              <div className="flex flex-row align-items">
+                <img src={Paw} alt="paw" className="mt-1 w-4" />
+                <p className="text-white pt-2 pl-1 text-[9px]">b/world news</p>
+              </div>
+            </div>
           </div>
 
-          <div className="bg-stone-700 max-w-xs p-3 rounded-md mx-1 my-1">
-            <h2 className="text-sm text-white">Nutrition</h2>
-            <h3 className="text-xs text-white">
-              Water bowl should be changed at least once daily.
-            </h3>
+          <div className=" relative shadow-md bg-stone-700 p-3 sm:max-w-[200px] max-w-[250px] min-h-[80px] rounded-md mx-1 my-1 ">
+            <img
+              src={dogDrink}
+              alt="puppy drinking"
+              className="
+              absolute 
+              inset-0 
+              h-full 
+              w-full 
+              object-cover
+              overflow-hidden
+              rounded-md
+              opacity-40"
+            />
+            <div className="relative">
+              <h2 className="text-sm font-bold mt-1 text-white">Hydration</h2>
+              <h3 className="text-xs text-white font-semi-bold">
+                Water bowl should be changed at least once daily.
+              </h3>
+              <div className="flex flex-row align-items">
+                <img src={Drop} alt="paw" className="mt-1 w-4" />
+                <p className="text-white pt-2 pl-1 text-[9px]">b/hydration</p>
+              </div>
+            </div>
           </div>
 
-          <div className="bg-stone-700 max-w-xs p-3 rounded-md mx-1 my-1">
-            <h2 className="text-sm text-white">Health & Exercise</h2>
-            <h3 className="text-xs text-white">
-              Recommended daily exercise for dogs is 30 minutes to 2 hours
-            </h3>
+          <div className="relative min-h-[80px] shadow-md bg-stone-700 sm:max-w-[200px] max-w-[250px] p-3 rounded-md mx-1 my-1">
+            <img
+              src={dogRun}
+              alt="dog race"
+              className="
+              absolute 
+              inset-0 
+              h-full 
+              w-full 
+              object-cover
+              overflow-hidden
+              rounded-md
+              opacity-40"
+            />
+            <div className="relative">
+              <h2 className="text-sm font-bold mt-1 text-white">
+                Cardiovascular
+              </h2>
+              <h3 className="text-xs text-white font-semi-bold">
+                Recommended daily exercise for dogs is 30 minutes to 2 hours
+              </h3>
+              <div className="flex flex-row align-items">
+                <img src={Flower} alt="paw" className="mt-1 w-4" />
+                <p className="text-white pt-2 pl-1 text-[9px]">
+                  b/health and exercise
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
