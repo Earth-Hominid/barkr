323158acad2ac6234f8be4faa39b447c6463400d
C <earthhominid@gmail.com>
2022-05-12 11:04:43 -0300

    added context to routes

--- client/src/routes/RouteSwitch.js
+++ client/src/routes/RouteSwitch.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { SiteContent } from '../context/SiteContent';
 import HomePage from './HomePage';
 import LogInPage from './LogInPage';
 import NotFoundPage from './NotFoundPage';
@@ -7,26 +8,32 @@ import SignUpPage from './SignUpPage';
 import WithNav from '../outlet/WithNav';
 
 const RouteSwitch = () => {
-  const [backendData, setBackendData] = useState([{}]);
+  const [messageData, setMessageData] = useState([{}]);
 
   useEffect(() => {
     fetch('/chatroom')
       .then((response) => response.json())
       .then((data) => {
-        setBackendData(data);
+        setMessageData(data);
       });
   }, []);
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<WithNav />}>
-          <Route path="/barker" element={<HomePage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/log-in" element={<LogInPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <SiteContent.Provider
+        value={{
+          messageData,
+        }}
+      >
+        <Routes>
+          <Route element={<WithNav />}>
+            <Route path="/barker" element={<HomePage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/log-in" element={<LogInPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </SiteContent.Provider>
     </BrowserRouter>
   );
 };
